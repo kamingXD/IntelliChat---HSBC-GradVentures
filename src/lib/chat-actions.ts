5b76115da121824d5e64aeb3ef7ee0c24a3cee41
Kaming Pong <peterpong0812@gmail.com>
2025-12-06 08:57:34 +0000

    Try fixing this error: `Console Error: Failed to fetch from https://gene

--- src/lib/chat-actions.ts
+++ src/lib/chat-actions.ts
@@ -21,7 +21,7 @@ const ChatRequestSchema = z.object({
 
 export async function continueConversation(history: Omit<Message, 'id'>[], prompt: string) {
     const llmResponse = await ai.generate({
-        model: 'googleai/gemini-1.5-flash',
+        model: 'googleai/gemini-pro',
         history: history,
         prompt: prompt,
         stream: true
